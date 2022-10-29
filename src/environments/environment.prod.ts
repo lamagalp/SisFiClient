672d75fbb10b1d284ba470b55e2581aaae0fc8bc
marina pinasco <marinapinasco@hotmail.com>
2022-10-29 18:14:15 -0300

    backend to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //API_URL:'https://sisfiserver.herokuapp.com/SisFi/api'
-  API_URL : 'https://sisfiserver.web.app/api'
+  API_URL:'https://sisfiserver.herokuapp.com/SisFi/api'
+  //API_URL : 'https://sisfiserver.web.app/api'
 };
